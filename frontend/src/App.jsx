678d4e2243708c82fbcbc8fe038a90f36e4da43d
Casandra Normand <casandra.normand@outlook.com>
2024-01-23 14:30:19 -0500

    Creating app router for multiple pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './components/AppRouter';
 import './App.css'
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-  
-  )
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 export default App
