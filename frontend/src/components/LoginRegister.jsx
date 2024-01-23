c16ed6d3edcbf94b8a67743f45ea247fcbea8ca2
Casandra Normand <casandra.normand@outlook.com>
2024-01-23 11:24:41 -0500

    Component files created

---
+++ frontend/src/components/LoginRegister.jsx
@@ -0,0 +1,23 @@
+return (
+  <section className="LoginRegisterForm">
+    <h1> Log in or register to pursue thy quest further </h1>
+    <div className="LoginForm">
+      <form method="post">
+        <textarea placeholder="Email or Username"></textarea>
+      </form>
+      <form method="post">
+        <textarea placeholder="Password"></textarea>
+      </form>
+    </div>
+    <div className="RegisterForm">
+    <form method="post">
+        <textarea placeholder="Email or Username"></textarea>
+      </form>
+      <form method="post">
+        <textarea placeholder="Password"></textarea>
+      </form>
+    </div>
+    <button> Submit </button>
+  </section>
+
+)
