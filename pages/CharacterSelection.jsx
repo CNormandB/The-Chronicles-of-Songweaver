3d0bc2e87f0f498d06a1a37910c1ff5b81fc4b90
Casandra Normand <casandra.normand@outlook.com>
2024-01-25 21:34:55 -0500

    Resizing characters

--- pages/CharacterSelection.jsx
+++ pages/CharacterSelection.jsx
@@ -1,9 +1,100 @@
-function CharacterSelection() {
-  return(
-    <section>
-      <p>hi</p>
-    </section>
-  )
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./CharacterSelection.css";
+import background from "/Locations/tavern.jpg";
+
+function ImageComponent() {
+  const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <div>
+    <img className="FemaleElfWizzard"
+      src="/Characters/FemaleElfWizzard.png"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+      style={{
+        opacity: hovered ? 0.7 : 1,
+        cursor: "pointer"
+      }}
+    />
+    <img className="MaleElfWizzard"
+      src= "/Characters/MaleElfWizzard.png"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+      style={{
+        opacity: hovered ? 0.7 : 1,
+        cursor: "pointer"
+      }}
+    />
+    <img className="FemaleHalf-OrcBarbarian"
+      src= "/Characters/FemaleHalf-OrcBarbarian.png" 
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+      style={{
+        opacity: hovered ? 0.7 : 1,
+        cursor: "pointer"
+      }}
+    />
+    <img className="MaleHalf-OrcBarbarian"
+      src= "/Characters/MaleHalf-OrcBarbarian.png" 
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+      style={{
+        opacity: hovered ? 0.7 : 1,
+        cursor: "pointer"
+      }}
+    />
+    <img className="FemaleGnomeRanger"
+      src= "/Characters/FemaleGnomeRanger.png" 
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+      style={{
+        opacity: hovered ? 0.7 : 1,
+        cursor: "pointer"
+      }}  
+    />
+    <img className="MaleGnomeRanger"
+      src= "/Characters/MaleGnomeRanger.png" 
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+      style={{
+        opacity: hovered ? 0.7 : 1,
+        cursor: "pointer"
+      }}  
+    />
+    </div>
+  );
 }
 
-export default CharacterSelection
+function CharacterSelection() {
+  return (
+    <section className="CharacterSelection" styles={{ backgroundImage: `url(${background})` }}>
+      <div className="Characters">
+        <ImageComponent />
+      </div>
+    </section>
+  );
+}
+
+export default CharacterSelection;
+
+
