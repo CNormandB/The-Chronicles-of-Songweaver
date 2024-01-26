db21b5125af49d7a9611fe948f22d042d12dbdf2
Casandra Normand <casandra.normand@outlook.com>
2024-01-26 09:02:01 -0500

    Imported the gameplay page into the app router

--- components/AppRouter.jsx
+++ components/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import CharacterSelection from '../pages/CharacterSelection';
+import Gameplay from '../pages/Gameplay';
 
 const AppRouter = () => {
   return (
