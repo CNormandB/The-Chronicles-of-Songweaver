f0130ac4c12834d18a67866fe3ea1c417050d46f
Stephanie Bowie"git config --global user.name Stephanie <steph.bowie1993@gmail.com>
2024-02-06 19:23:54 -0700

    fix character stats for rolls

--- src/data/characters.js
+++ src/data/characters.js
@@ -1,46 +1,46 @@
 const characters = [
-    {
-      class: 'Barbarian',
-      race: 'Half-Orc',
-      stats: {
-        strength: 16,
-        dexterity: 12,
-        constitution: 14,
-        intelligence: 8,
-        wisdom: 10,
-        charisma: 10,
-      },
-      hitPoints: 12,
-      armorClass: 14,
+  {
+    class: 'Barbarian',
+    race: 'Half-Orc',
+    stats: {
+      strength: 4,
+      dexterity: 1,
+      constitution: 2,
+      intelligence: -1,
+      wisdom: 0,
+      charisma: 0,
     },
-    {
-        class: 'Wizard',
-        race: 'High-Elf',
-        stats: {
-          strength: 8,
-          dexterity: 14,
-          constitution: 12,
-          intelligence: 16,
-          wisdom: 10,
-          charisma: 10,
-        },
-        hitPoints: 12,
-        armorClass: 12,
-      },
-      {
-        class: 'Rogue',
-        race: 'Tiefling',
-        stats: {
-          strength: 8,
-          dexterity: 14,
-          constitution: 12,
-          intelligence: 16,
-          wisdom: 10,
-          charisma: 10,
-        },
-        hitPoints: 12,
-        armorClass: 12,
-      },
-  ];
+    hitPoints: 12,
+    armorClass: 14,
+  },
+  {
+    class: 'Wizard',
+    race: 'High-Elf',
+    stats: {
+      strength: -1,
+      dexterity: 2,
+      constitution: 1,
+      intelligence: 4,
+      wisdom: 0,
+      charisma: 0,
+    },
+    hitPoints: 12,
+    armorClass: 12,
+  },
+  {
+    class: 'Rogue',
+    race: 'Tiefling',
+    stats: {
+      strength: -1,
+      dexterity: 2,
+      constitution: 1,
+      intelligence: 4,
+      wisdom: 0,
+      charisma: 0,
+    },
+    hitPoints: 12,
+    armorClass: 12,
+  },
+];
   
   export default characters;
