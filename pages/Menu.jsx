2fc8d70de2e4ead717f40e51d3d9831623487e27
Casandra Normand <casandra.normand@outlook.com>
2024-02-08 19:37:55 -0500

    menu page outline

---
+++ pages/Menu.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <div className="Menu">
+      <img className="logo"></img>
+      <img className="Songweaver"></img>
+      <button className="StartNew"></button>
+      <button className="LoadGame"></button>
+      <button></button>
+    </div>
+  );
+}
+
+export default Menu;
