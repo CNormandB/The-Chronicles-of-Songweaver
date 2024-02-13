0b328639c698e9d013edcc79d1a19354b5d0071e
Casandra Normand <casandra.normand@outlook.com>
2024-02-12 23:13:41 -0500

    Updated header styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <section className='Header'>
+    <section className="Header">
       <img src="HeaderLogo.png" className="Logo"></img>
-      <p className='Greeting'>Hello **Player**</p>
+      <div className="RightSide">
+        <p className="Greeting">Hello **Player**</p>
+        <button className="Logout">Logout</button>
+      </div>
     </section>
   );
 };
