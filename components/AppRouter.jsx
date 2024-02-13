6fe1a933ae929389af64692e37a2d930a4cde430
Casandra Normand <casandra.normand@outlook.com>
2024-02-13 03:44:42 -0500

    add menu to router

--- components/AppRouter.jsx
+++ components/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Menu from '../pages/Menu'
 import CharacterSelection from '../pages/CharacterSelection';
 import Gameplay from '../pages/Gameplay';
 
